e41ba1fae1e63a3c5efa4fb563f5984cd935365b
aiguo-zhao <2860729247@qq.com>
2024-08-26 16:53:40 +0800

    8821 uart blue
    Signed-off-by: aiguo-zhao <2860729247@qq.com>

---
+++ drivers/misc/bluetooth_uart_driver/.built-in.a.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/misc/bluetooth_uart_driver/built-in.a :=  rm -f drivers/misc/bluetooth_uart_driver/built-in.a; ./../prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-ar rcSTPD drivers/misc/bluetooth_uart_driver/built-in.a 
