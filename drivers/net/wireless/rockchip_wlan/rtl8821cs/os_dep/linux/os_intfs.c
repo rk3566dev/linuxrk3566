509060572d13645a0348e447a19060921e860885
aiguo-zhao <2860729247@qq.com>
2025-04-30 01:39:46 +0800

    fix wlan0 wlan1
    Signed-off-by: aiguo-zhao <2860729247@qq.com>

--- drivers/net/wireless/rockchip_wlan/rtl8821cs/os_dep/linux/os_intfs.c
+++ drivers/net/wireless/rockchip_wlan/rtl8821cs/os_dep/linux/os_intfs.c
@@ -511,14 +511,14 @@ int rtw_nm = 1;/*noise monitor*/
 module_param(rtw_nm, int, 0644);
 #endif
 
-char *ifname = "wlan%d";
+char *ifname = "wlan1";
 module_param(ifname, charp, 0644);
 MODULE_PARM_DESC(ifname, "The default name to allocate for first interface");
 
 #ifdef CONFIG_PLATFORM_ANDROID
 //	char *if2name = "p2p%d";
 //#else 
-	char *if2name = "wlan%d";
+	char *if2name = "wlan0";
 #endif /* CONFIG_PLATFORM_ANDROID */
 module_param(if2name, charp, 0644);
 MODULE_PARM_DESC(if2name, "The default name to allocate for second interface");
